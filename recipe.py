922e31ee0330b2f45169f222c70f5f5947b30cdc
Ubuntu <ubuntu@ip-172-31-43-5.ec2.internal>
2019-08-14 16:13:49 +0000

    Python Library files added = os, math, re and json to ensure Python3 fully responds to project code

---
+++ recipe.py
@@ -0,0 +1,15 @@
+import os
+import math
+import re
+import json
+from flask import Flask, render_template, redirect, request, url_for, flash
+from flask_pymongo import PyMongo, pymongo # enables Flask to connect to MongoDB Atlas
+from bson.objectid import ObjectId 
+from werkzeug.utils import secure_filename
+
+"""
+MongoDB represents JSON documents in binary-encoded format called BSON behind the scenes.
+BSON extends the JSON model to provide additional data types, ordered fields, and to be
+efficient for encoding and decoding within different languages.
+"""
+
