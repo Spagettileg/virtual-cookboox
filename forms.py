dea9a5e191809247f5948604fd1dca65b327856b
Ubuntu <ubuntu@ip-172-31-43-5.ec2.internal>
2019-09-17 11:47:39 +0000

    Flaskforms, CSRFprotect, wtforms imported libraries to support user authentication to virtual cookbook

--- forms.py
+++ forms.py
@@ -1,7 +1,25 @@
+import os
+from flask import Flask
 from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
 from wtforms import StringField, TextAreaField, IntegerField, PasswordField
 from wtforms.validators import DataRequired, ValidationError, Optional
+from flask_pymongo import PyMongo, pymongo
 
+csrf = CSRFProtect()
+
+def create_app():
+    csrf.init_app(app)
+
+app = Flask(__name__)    
+"""
+Environment variables SECRET and MONGO_URI set in Heroku
+dashboard in production.
+"""
+app.secret_key = os.getenv("SECRET")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["MONGO_DBNAME"] = "virtual_cookbook"
+mongo = PyMongo(app)
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -14,14 +32,16 @@ class RegistrationForm(FlaskForm):
 
 
 class RecipeForm(FlaskForm):
-    recipe_name = StringField('Recipe Name', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[DataRequired()])
-    health_labels = TextAreaField('Health Labels', validators=[Optional()])
-    diet_labels = TextAreaField('Diet Labels', validators=[Optional()])
-    ingredients = TextAreaField('Ingredients', validators=[DataRequired()])
-    cooking_time = IntegerField('Cooking Time', validators=[DataRequired()])
-    serving_size = IntegerField('Serving Size', validators=[DataRequired()])
-    calories = IntegerField('Calories', validators=[DataRequired()])
-    recipe_image = StringField(
-        'Copy Image Address Link', validators=[DataRequired()])
-    source = StringField('Source URL Link', validators=[Optional()])
+    category_name = StringField('category_name', validators=[DataRequired()])
+    complexity = StringField('complexity', validators=[DataRequired()])
+    recipe_name = TextAreaField('recipe_name', validators=[DataRequired()])
+    author_name = TextAreaField('author_name', validators=[DataRequired()])
+    prep_time_mins = IntegerField('prep_time_mins', validators=[DataRequired()])
+    cook_time_mins = IntegerField('cook_time_mins', validators=[DataRequired()])
+    calories = IntegerField('calories', validators=[DataRequired()])
+    servings = IntegerField('servings', validators=[DataRequired()])
+    brief_description = TextAreaField('brief_description', validators=[DataRequired()])
+    ingredients = TextAreaField('ingredients', validators=[DataRequired()])
+    instructions = TextAreaField('instructions', validators=[DataRequired()])
+    recipe_image = StringField('recipe_image', validators=[DataRequired()])
+    favourite = StringField('favourite', validators=[Optional()])
