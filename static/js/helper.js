3f913b89a660b3b7dddf92ed763aa1d3e985b99a
Ubuntu <ubuntu@ip-172-31-60-195.ec2.internal>
2020-06-03 13:50:33 +0000

    New developers years function added that supports footer information.

--- static/js/helper.js
+++ static/js/helper.js
@@ -23,5 +23,13 @@ var l = areas.length;while (l--) {
  makeExpandingArea(areas[l]);
 }
 
+$(document).ready(function () {
+
+    // Website authors developer years appear in the footer
+     
+    $('#developer-years').text('2018 - ' + new Date().getFullYear());
+    
+});
+
 
 
