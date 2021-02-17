7a4d590de54f6d7bcdc669166f81016e651e3502
Ubuntu <ubuntu@ip-172-31-36-140.eu-west-1.compute.internal>
2021-02-17 14:27:53 +0000

    debug reset to False for production purposes.

--- recipe.py
+++ recipe.py
@@ -427,4 +427,4 @@ def logout():
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP', "0.0.0.0"),
             port=int(os.environ.get('PORT', "5000")),
-            debug=True)
+            debug=False)
